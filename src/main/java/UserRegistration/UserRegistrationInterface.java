776c20f364754658c76dc13dcec2d931abef70a0
Priyaksm12 <priyankaks.priya@gmail.com>
2022-01-12 22:40:49 +0530

    Day21st Assignment

---
+++ src/main/java/UserRegistration/UserRegistrationInterface.java
@@ -0,0 +1,5 @@
+package UserRegistration;
+@FunctionalInterface
+public interface UserRegistrationInterface {
+    public abstract boolean userEntries(String value) throws InvalidUserInputException;
+}
